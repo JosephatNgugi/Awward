cc1fa31d979afddc4092cfb723fade0dc7738710
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-15 13:47:01 +0300

    create a profile model class

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    profile_picture = CloudinaryField('images', default='default.png')
+    bio = models.TextField(max_length=500, default="My Bio", blank=True)
+    name = models.CharField(max_length=120)
+    location = models.CharField(max_length=60, blank=True)
+    contact = models.EmailField(max_length=100, blank=True)
+    joined = models.DateTimeField(default=timezone.now)

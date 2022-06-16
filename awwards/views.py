88f8dd6c37b1868bf41e298e325ebc409d0a4256
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-16 14:18:08 +0300

    a method to display projects queried from database

--- awwards/views.py
+++ awwards/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
 
 # Create your views here.
+def homepage(request):
+    projects = Project.all_projects()
+    return render(request, 'awwards/homepage.html', {"projects":projects})

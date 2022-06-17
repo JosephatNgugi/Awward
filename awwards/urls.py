4be554720f922cd68e0b8e32eed2c911bb582501
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-17 19:25:51 +0300

    add redirect view of user upon login to fix page not found, Fix logout to enable a user to logout

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from django.urls import include, path
 
 from . import views
@@ -8,6 +9,8 @@ urlpatterns = [
     path('', views.homepage, name='home'),
     path('accounts/Sign-Up/', views.UserRegistration, name='signup'),
     path('accounts/profile/<id>/',views.profile,name = 'profile'),
+    path('accounts/profile/', RedirectView.as_view(url='/')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/profile-update/',views.edit_profile,name = 'editprofile'), 
     path('newproject', views.add_project, name='addProject'),
     path('projects/<id>/',views.projects,name = 'projects'),
@@ -15,8 +18,6 @@ urlpatterns = [
     path('rate/<id>/',views.ratings,name = 'rate'),
     path('api/projects/', views.ProjectList.as_view()),
     path('api/profile/', views.ProfileList.as_view()),
-
-
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
